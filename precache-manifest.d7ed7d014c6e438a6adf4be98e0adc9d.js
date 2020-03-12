4dedb2202baaebdce80584e7037ee0cc65dda3a2
unknown <khuongitptit@gmail.com>
2020-03-12 11:10:46 +0700

    Updates

---
+++ precache-manifest.d7ed7d014c6e438a6adf4be98e0adc9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12382ce5556a0a1eb78f3c776812cfb9",
+    "url": "/lol-react-app/index.html"
+  },
+  {
+    "revision": "cf68668bea04fc92bbba",
+    "url": "/lol-react-app/static/css/main.7a429e6a.chunk.css"
+  },
+  {
+    "revision": "de5cd451caa45bf12f76",
+    "url": "/lol-react-app/static/js/2.08519c41.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lol-react-app/static/js/2.08519c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf68668bea04fc92bbba",
+    "url": "/lol-react-app/static/js/main.92630318.chunk.js"
+  },
+  {
+    "revision": "1682c9f41fb327f2f25c",
+    "url": "/lol-react-app/static/js/runtime-main.d1c09f43.js"
+  },
+  {
+    "revision": "5ece7df8cadb026a4e31113181e499a2",
+    "url": "/lol-react-app/static/media/bg.5ece7df8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5369aa683fc7535ca10890492698cb62.js"
+  "/lol-react-app/precache-manifest.d7ed7d014c6e438a6adf4be98e0adc9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lol-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
